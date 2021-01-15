a41e9cfa6f9c8ae98460a26dc9506929694db422
Divye <74274049+bblDiv@users.noreply.github.com>
2021-01-14 20:04:36 -0500

    Add files via upload

---
+++ asynchronousBallMovement-master/sketch.js
@@ -0,0 +1,43 @@
+var ball,database,position;
+
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var ballPosition=database.ref('ball/position');
+    ballPosition.on("value",readPosition,showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-5,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(5,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-5);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+5);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+database.ref('ball/position').set({
+    'x':position.x+x,
+    'y':position.y+y
+})
+}
+function readPosition(data){
+    position=data.val();
+    ball.x=position.x;
+    ball.y=position.y;
+    console.log(position.x);
+}
+function showError(){
+console.log("jfjfjfjfjjfjfjfjfj");
+}
